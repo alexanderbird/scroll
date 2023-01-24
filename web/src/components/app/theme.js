7fc31bd814bf45bf264a849ad7bddf6cb023c9f1
Alexander Bird <alex@alexanderbird.software>
2023-01-24 09:02:49 -0800

    Update the theme color in the manifest.json
    Also refactor so it's obvious that manifest.json should be changed when theme.js is changed

--- web/src/components/app/theme.js
+++ web/src/components/app/theme.js
@@ -1,10 +1,11 @@
 import { createTheme } from "@mui/material/styles";
+import * as manifest from '../../manifest.json';
 
 export const theme = createTheme({
   palette: {
     type: 'light',
     primary: {
-      main: '#001f3d',
+      main: manifest.theme_color,
     },
     secondary: {
       main: '#98d7c2',
